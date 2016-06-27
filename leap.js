9e8fc5d9b9a50d79bfe22a12b61ed8e698d52c97
pamsny <pamela.garza.sny@gmail.com>
2016-06-26 23:19:06 -0500

    Update leap.js

--- leap.js
+++ leap.js
@@ -1,1 +1,16 @@
 # daily-challenges
+var year = prompt('Enter a year');
+function leapYear(year) 
+{
+
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0){
+        return console.log(year + ' is a leap year!!!');         
+    }
+    else {
+        return console.log(year + ' is NOT a leap year');        
+    }
+
+}
+
+document.write(leapYear(year));
+//console.log(leapYear(year));
